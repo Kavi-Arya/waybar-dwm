e8708c4a25f86e67172154e2d503670d2cd4d6e0
Kavi Arya <kaviarya047@tutamail.com>
2025-02-17 02:32:33 +0530

    updates

--- scripts/nowPlaying.sh
+++ scripts/nowPlaying.sh
@@ -1,10 +1,10 @@
 #!/bin/sh
 
 Status=$(playerctl status)
-Title=$(playerctl metadata --format '{{title}}' | sed -E 's/^(.{40}).*$/\1.../' 2>/dev/null)
-Artist=$(playerctl metadata --format '{{artist}}' | sed -E 's/^(.{40}).*$/\1.../' 2>/dev/null)
+Title=$(playerctl metadata --format '{{title}}' | sed -E 's/^(.{60}).*$/\1.../' 2>/dev/null)
+Artist=$(playerctl metadata --format '{{artist}}' | sed -E 's/^(.{60}).*$/\1.../' 2>/dev/null)
 windowTitle () {
   VAR=$(hyprctl activewindow -j)
-  echo $VAR | grep -oP '"title":\s*"\K[^"]+' | sed -E 's/^(.{40}).*$/\1.../' || echo "~"
+  echo $VAR | grep -oP '"title":\s*"\K[^"]+' | sed -E 's/^(.{90}).*$/\1.../' || echo "~"
 }
-[[ $Status == *"Playing"* ]] && echo "Now Playing: $Artist - $Title" || exit
+[[ $Status == *"Playing"* ]] && echo "$Artist: $Title" || windowTitle
